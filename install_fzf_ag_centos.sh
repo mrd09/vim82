99bd539420830ab6685230904700de360b467a7c
MrD09 <hgdat92@gmail.com>
2020-04-23 06:18:37 +0000

    git: bash-git-prompt + alias

--- install_fzf_ag_centos.sh
+++ install_fzf_ag_centos.sh
@@ -2,5 +2,7 @@
 
 sudo yum install -y the_silver_searcher && \
 
+git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1 && \
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && \
 ~/.fzf/install
